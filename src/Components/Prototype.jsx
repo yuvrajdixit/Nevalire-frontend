288757aa5235addcc24b097c9004803aaef63ac0
Yuvraj Dixit <61179188+yuvrajdixit@users.noreply.github.com>
2025-08-04 16:12:38 +0530

    Update Prototype.jsx

--- src/Components/Prototype.jsx
+++ src/Components/Prototype.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import mockup from '\src\Assets\Screenshot 2025-08-02 191309.png';
 const Prototype = () => {
   return (
     <div className='Prototype-container'>
         <h2>Nevalire.</h2>
         <div className="image-box">
-        <img src="\src\Assets\Screenshot 2025-08-02 191309.png" alt="Descriptive text" />
+        <img src={mockup} alt="Descriptive text" />
         </div>
     </div>
   )
